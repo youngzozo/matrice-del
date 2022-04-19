1ae7fac41dae1d28b5971308d999d3948dd4b0e6
youngzozo <103504453+youngzozo@users.noreply.github.com>
2022-04-19 13:24:39 +0200

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,49 @@
+basic.forever(function () {
+    basic.showLeds(`
+        . . # . .
+        . # # # .
+        # . # . #
+        . . # . .
+        . . # . .
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        . # # # .
+        # . # . #
+        . . # . .
+        . . # . .
+        . . . . .
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        # . # . #
+        . . # . .
+        . . # . .
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        . . # . .
+        . . # . .
+        . . . . .
+        . . . . .
+        . . # . .
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        . . # . .
+        . . . . .
+        . . . . .
+        . . # . .
+        . # # # .
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . # . .
+        . # # # .
+        # . # . #
+        `)
+})
